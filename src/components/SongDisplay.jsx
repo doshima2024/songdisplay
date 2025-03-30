e83d73611700b9f1ae5b1b44a7b4fcd7716dba06
Daniel Oshima <doshima2013@gmail.com>
2025-03-30 11:11:01 -0400

    add useEffect to store ratings in state upon mount

--- src/components/SongDisplay.jsx
+++ src/components/SongDisplay.jsx
@@ -11,17 +11,24 @@ function SongDisplay() {
         .then(data => setSongs(data))
         .catch(error => setError(error.message))
     }, [])  
+
+   useEffect(() => {
+        fetch("http://127.0.0.1:5000/ratings")
+        .then (response => response.json())
+        .then (data => setRatings(data))
+        .catch (error => setError(error.message))
+   }, [])
    
 
     return(
         <div>
             <h1>Songs And Ratings</h1>
-            
+                {error && <p>Error: {error}</p>}
                 
                 {songs.map((song) => <div key={song.id}>
                     <h2>{song.name} by {song.artist}</h2>
                     <iframe src={`https://open.spotify.com/embed/track/${song.url.split("/").pop()}`} width="300" height="80"></iframe>
-                </div>)}:
+                </div>)}
             
         </div>
     )
