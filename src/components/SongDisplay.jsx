2d72933f2dbdcb0663adad9e96f13ccf6e156d30
Daniel Oshima <doshima2013@gmail.com>
2025-03-30 13:16:49 -0400

    add JSX logic to display ratings under each related song

--- src/components/SongDisplay.jsx
+++ src/components/SongDisplay.jsx
@@ -19,16 +19,23 @@ function SongDisplay() {
         .catch (error => setError(error.message))
    }, [])
    
+   
 
     return(
         <div>
             <h1>Songs And Ratings</h1>
                 {error && <p>Error: {error}</p>}
                 
-                {songs.map((song) => <div key={song.id}>
+                {songs.map((song) => {
+                    const songRatings = ratings.filter(rating => rating.song_id === song.id)
+                return(
+                    <div key={song.id}>
                     <h2>{song.name} by {song.artist}</h2>
                     <iframe src={`https://open.spotify.com/embed/track/${song.url.split("/").pop()}`} width="300" height="80"></iframe>
-                </div>)}
+                    {songRatings.map((rating) =>
+                    <p key={rating.id}>Song Rating: {rating.rating}</p>)}
+                </div>)
+            })}
             
         </div>
     )
