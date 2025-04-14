395e5335eb0ced5ec5b5f1cf64d0b6c145f5a7ba
Daniel Oshima <doshima2013@gmail.com>
2025-04-14 18:10:10 -0400

    Add validation methods to Song model (for name, artist and url)

--- server/models.py
+++ server/models.py
@@ -1,5 +1,6 @@
 from server.app import db
 from flask import jsonify
+from sqlalchemy.orm import validates
 
 class Song(db.Model):
 
@@ -10,13 +11,37 @@ class Song(db.Model):
     artist = db.Column(db.String, nullable=False)
     url = db.Column(db.String, nullable=False)
 
-    ratings = db.relationship("Rating", back_populates="song")
+    ratings = db.relationship("Rating", back_populates="song", cascade="all, delete")
 
     def __repr__(self): 
         return f'Song: {self.name} is by {self.artist}'
     
     def to_dict(self):
         return {"id": self.id, "name": self.name, "artist": self.artist, "url": self.url}
+      
+    @validates("name")
+    def validate_name(self, key, value):
+        if not type(value) == str:
+            raise TypeError("song name must be a string")
+        if not value.strip():
+            raise ValueError("song name must not be empty")
+        return value
+    
+    @validates("artist")
+    def validate_artist(self, key, value):
+        if not type(value) == str:
+            raise TypeError("artist name must be a string")
+        if not value.strip():
+            raise ValueError("artist name must not be empty")
+        return value
+    
+    @validates("url")
+    def validate_url(self, key, value):
+        if not type(value) == str:
+            raise TypeError("URL must be a string")
+        if not value.strip():
+            raise ValueError("URL must not be empty")
+        return value
 
 class Rating(db.Model):
 
