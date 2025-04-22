b1fdcd0a98beedcb9bb19c27ce6f2f4afb02fc02
Daniel Oshima <doshima2013@gmail.com>
2025-04-22 17:35:01 -0400

    Style Song Display and Add Song cards with bg-white color

--- src/components/SongDisplay.jsx
+++ src/components/SongDisplay.jsx
@@ -50,14 +50,14 @@ function SongDisplay() {
     return(
         <div  className="rounded bg-gray-300 text-gray-700 max-w-5xl p-50"> 
         <div className="space-y-10">
-            <h1 className="text-3xl font-bold text-center mb-6">Songs And Ratings</h1>
+            <h1 className="text-4xl font-bold text-center text-gray-800">Songs And Ratings</h1>
 
                 {error && <p className="text-red-500 font-semibold text-center">Error: {error}</p>}
                 
                 {songs.map((song) => {
                     const songRatings = ratings.filter(rating => rating.song_id === song.id)
                 return(
-                    <div key={song.id} className="bg-gray-200 rounded-lg p-4 shadow-sm space-y-4">
+                    <div key={song.id} className="bg-white rounded-xl p-6 shadow-md space-y-4">
                     <h2 className="text-xl font-semibold">{song.name} by {song.artist}</h2>
                     <iframe src={`https://open.spotify.com/embed/track/${song.url.split("/").pop()}`} width="300" height="80" className="mx-auto w-full"></iframe>
                     <br></br>
