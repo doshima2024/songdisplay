7f3d5943f923950300dbe6c2f0a75f510d685c5f
Daniel Oshima <doshima2013@gmail.com>
2025-04-30 17:10:45 -0400

    Add client side validations to Add Rating form to ensure rating is between 1 and 10

--- src/components/SongDisplay.jsx
+++ src/components/SongDisplay.jsx
@@ -33,6 +33,13 @@ function SongDisplay() {
     
     event.preventDefault();
 
+    const ratingValue = newRating[id];
+
+    if (!ratingValue || ratingValue < 1 || ratingValue > 10) {
+        alert("Please enter a rating between 1 and 10");
+        return;
+    }
+
     fetch(`http://127.0.0.1:5000/ratings/${id}`, {"method": "POST",
         headers: {"Content-Type": "application/json",
             "Accept": "application/json"
@@ -50,14 +57,14 @@ function SongDisplay() {
     return(
         <div  className="rounded bg-gray-300 text-gray-700 max-w-5xl p-50"> 
         <div className="space-y-10">
-            <h1 className="text-4xl font-bold text-center text-gray-800">Songs And Ratings</h1>
+            <h1 className="text-3xl font-bold text-center mb-6">Songs And Ratings</h1>
 
                 {error && <p className="text-red-500 font-semibold text-center">Error: {error}</p>}
                 
                 {songs.map((song) => {
                     const songRatings = ratings.filter(rating => rating.song_id === song.id)
                 return(
-                    <div key={song.id} className="bg-white rounded-xl p-6 shadow-md space-y-4">
+                    <div key={song.id} className="bg-gray-200 rounded-lg p-4 shadow-sm space-y-4">
                     <h2 className="text-xl font-semibold">{song.name} by {song.artist}</h2>
                     <iframe src={`https://open.spotify.com/embed/track/${song.url.split("/").pop()}`} width="300" height="80" className="mx-auto w-full"></iframe>
                     <br></br>
