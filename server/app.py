52b506900a472213b1df32d436270e1dc0167a3b
Daniel Oshima <doshima2013@gmail.com>
2025-05-09 16:34:51 -0400

    Update database configuration line in app.py

--- server/app.py
+++ server/app.py
@@ -2,11 +2,12 @@ from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_cors import CORS
+import os
 
 
 app = Flask(__name__)
 CORS(app)
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "sqlite:///main.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL", "postgresql://apple:apple@localhost:5432/songdisplaydb")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 
