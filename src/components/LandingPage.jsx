603265db0d48de0bb7c11b1ae4b042b8964d4b65
Daniel Oshima <doshima2013@gmail.com>
2025-06-01 19:06:03 -0400

    Add style to NavBar

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function LandingPage() {
 
 return(
-    <div className="rounded bg-gray-300 text-gray-700 max-w-5xl p-50">
+    <div className="rounded bg-gray-300 text-gray-700 max-w-5xl mx-auto p-10 space-y-6 shadow-md">
         <h1 className="text-3xl font-bold text-center mb-6">Welcome To The Song Display and Rater</h1>
         <h3 className="bg-gray-200 rounded-lg p-4 shadow-sm space-y-4">In this app, you can add your favorite song from Spotify to the community board and rate other users entries. <br></br> It is a community to 
             share music and share and explore other's opinions of our favorite songs!
